ca9f929857ee6fba42f4f7afc0b8ebee058d3bb6
billqualls7 <81820683+billqualls7@users.noreply.github.com>
2024-04-26 23:51:37 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rqh/wheel4robot/communication
-# Build directory: /home/rqh/wheel4robot/communication/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
